32d00a0478bb42c72c24efe0bcdfda240e926836
MrKelpy <alexandresilva.coding@gmail.com>
2023-04-30 17:40:51 +0100

    Prepared the repo for merge with main branch

--- gui/LoadingScreen.Designer.cs
+++ gui/LoadingScreen.Designer.cs
@@ -30,55 +30,51 @@ namespace MCSMLauncher.gui
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
-            this.PictureBoxLoading = new System.Windows.Forms.PictureBox();
-            this.LabelStatus = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLoading)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // PictureBoxLoading
-            // 
-            this.PictureBoxLoading.BackColor = System.Drawing.Color.Transparent;
-            this.PictureBoxLoading.Location = new System.Drawing.Point(722, 12);
-            this.PictureBoxLoading.Name = "PictureBoxLoading";
-            this.PictureBoxLoading.Size = new System.Drawing.Size(66, 62);
-            this.PictureBoxLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.PictureBoxLoading.TabIndex = 0;
-            this.PictureBoxLoading.TabStop = false;
-            // 
-            // LabelStatus
-            // 
-            this.LabelStatus.BackColor = System.Drawing.Color.Transparent;
-            this.LabelStatus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LabelStatus.Location = new System.Drawing.Point(4, 393);
-            this.LabelStatus.Name = "LabelStatus";
-            this.LabelStatus.Size = new System.Drawing.Size(673, 18);
-            this.LabelStatus.TabIndex = 1;
-            this.LabelStatus.Text = "TEXT GOES HERE";
-            // 
-            // LoadingScreen
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 413);
-            this.Controls.Add(this.LabelStatus);
-            this.Controls.Add(this.PictureBoxLoading);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "LoadingScreen";
-            this.Text = "LoadingScreen";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoadingScreen_FormClosing);
-            this.Load += new System.EventHandler(this.LoadingScreen_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLoading)).EndInit();
-            this.ResumeLayout(false);
-        }
+{
+    System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
+    this.PictureBoxLoading = new System.Windows.Forms.PictureBox();
+    this.LabelStatus = new System.Windows.Forms.Label();
+    ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLoading)).BeginInit();
+    this.SuspendLayout();
+    // 
+    // PictureBoxLoading
+    // 
+    this.PictureBoxLoading.BackColor = System.Drawing.Color.Transparent;
+    this.PictureBoxLoading.Location = new System.Drawing.Point(722, 12);
+    this.PictureBoxLoading.Name = "PictureBoxLoading";
+    this.PictureBoxLoading.Size = new System.Drawing.Size(66, 62);
+    this.PictureBoxLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+    this.PictureBoxLoading.TabIndex = 0;
+    this.PictureBoxLoading.TabStop = false;
+    // 
+    // LabelStatus
+    // 
+    this.LabelStatus.BackColor = System.Drawing.Color.Transparent;
+    this.LabelStatus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+    this.LabelStatus.Location = new System.Drawing.Point(4, 393);
+    this.LabelStatus.Name = "LabelStatus";
+    this.LabelStatus.Size = new System.Drawing.Size(673, 18);
+    this.LabelStatus.TabIndex = 1;
+    this.LabelStatus.Text = "TEXT GOES HERE";
+    // 
+    // LoadingScreen
+    // 
+    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+    this.ClientSize = new System.Drawing.Size(800, 413);
+    this.Controls.Add(this.LabelStatus);
+    this.Controls.Add(this.PictureBoxLoading);
+    this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+    this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+    this.Name = "LoadingScreen";
+    this.Text = "LoadingScreen";
+    this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoadingScreen_FormClosing);
+    this.Load += new System.EventHandler(this.LoadingScreen_Load);
+    ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLoading)).EndInit();
+    this.ResumeLayout(false);
+}
 
         private System.Windows.Forms.Label LabelStatus;
 
